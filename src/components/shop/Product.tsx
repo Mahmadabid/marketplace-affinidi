393c4181930c74d909fff6288daa16806eff2c60
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-02-19 20:59:11 +0500

    Update Product.tsx

--- src/components/shop/Product.tsx
+++ src/components/shop/Product.tsx
@@ -16,15 +16,17 @@ const Product: React.FC<ProductProps> = ({product, openModal}) => {
     const { addToCart } = useCartContext();
 
     return (
-        <div className="border-2 border-gray-300 p-4 text-center flex-1 max-w-60 min-w-60 bg-white mx-6 rounded-lg overflow-hidden mb-6">
-            <img src={product.imageUrl} alt={product.name} className="max-h-40 max-w-40 mx-auto mb-4" />
-            <h2 className="text-lg font-semibold">{product.name}</h2>
-            <p className="text-gray-600 font-medium">
-                {User.user.verified && <span className='line-through text-red-500'>
-                    {country.currencySymbol}{convertedPrice(product.price, country.currencyRate)}
-                </span>}&nbsp;
-                {country.currencySymbol}{discountedPrice(convertedPrice(product.price, country.currencyRate), User.user.verified || false)}
-            </p>
+        <div className="border-2 border-gray-300 p-4 text-center flex-1 max-w-60 min-w-60 bg-white mx-6 rounded-lg overflow-hidden mb-6 flex flex-col justify-between">
+            <div>
+                <img src={product.imageUrl} alt={product.name} className="max-h-40 max-w-40 mx-auto mb-4" />
+                <h2 className="text-lg font-semibold">{product.name}</h2>
+                <p className="text-gray-600 font-medium">
+                    {User.user.verified && <span className='line-through text-red-500'>
+                        {country.currencySymbol}{convertedPrice(product.price, country.currencyRate)}
+                    </span>}&nbsp;
+                    {country.currencySymbol}{discountedPrice(convertedPrice(product.price, country.currencyRate), User.user.verified || false)}
+                </p>
+            </div>
             <button
                 onClick={() => addToCart({ ...product, price: User.user.verified ? discountedPrice(product.price, User.user.verified) : product.price }, openModal)}
                 className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded mt-4"
@@ -35,4 +37,4 @@ const Product: React.FC<ProductProps> = ({product, openModal}) => {
     )
 }
 
-export default Product
+export default Product;
